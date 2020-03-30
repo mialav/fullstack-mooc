81ee75a81adbc12f1d4539e318e049bbe484e419
Mia Lavanti <mialavanti@Mias-MacBook-Air-3.local>
2020-03-30 18:48:46 +0300

    unicafe part 2 added

--- pt-1/unicafe/src/index.js
+++ pt-1/unicafe/src/index.js
@@ -23,6 +23,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+  const average = good * 1 + neutral * 0 + (bad * -1) / total;
+  const positives = good / total;
 
   // console.log("good ", good);
   // console.log("neutral ", neutral);
@@ -38,6 +41,9 @@ const App = () => {
       <Stat text="good" amount={good} />
       <Stat text="neutral" amount={neutral} />
       <Stat text="bad" amount={bad} />
+      <Stat text="all" amount={total} />
+      <Stat text="average" amount={average ? average : 0} />
+      <Stat text="positive" amount={positives ? positives * 100 + "%" : "-"} />
     </div>
   );
 };
